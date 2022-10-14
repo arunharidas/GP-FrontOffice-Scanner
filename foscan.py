3ae692e84e3fc0cfc024a756cf7e9b86230d8b3e
arunharidas <94458457+arunharidas@users.noreply.github.com>
2022-10-15 00:40:51 +0530

    Add files via upload

--- foscan.py
+++ foscan.py
@@ -1,12 +1,29 @@
-# GP Front Office scanner
+"""
+    Front Office Scanner application for Gramapanchayat ILGMS
+    Copyright (C) 2022  Arun Haridas
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 import string
 import os
+import webbrowser
 
-AppVersion="0.1 (Alpha edition)"
+AppVersion="0.2 (Alpha edition)"
 window = Tk()
 window.geometry("400x380")
 window.resizable(False, False)
@@ -44,8 +61,8 @@ def ScanNow():                                                                  
         messagebox.showerror("Error", "Please check application name and Document type")
     else:
         if tempvar == 1:                                # Normal Scan
-            cmd='NAPS2.Console.exe -o "Scanned\\' + varFileName +'" --progress --profile ilgms-n'
-            os.system(cmd)                                                                                                #xxxxxxxxxxxxx test cmd/c
+            cmd='NAPS2.Console.exe -o "Scanned\\' + varFileName +'" --progress'
+            os.system(cmd)
         elif tempvar == 2:                              # Text Scan
             cmd='NAPS2.Console.exe -o "Scanned\\' + varFileName +'" --progress --profile ilgms-t'
             os.system(cmd)  
@@ -55,6 +72,22 @@ def ScanNow():                                                                  
         else:
             messagebox.showerror("Warning", "Please select Scan Type")
 
+def openscanned():
+    cmd='explorer scanned\\'
+    os.system(cmd)  
+
+def clearscanned():
+    cmd='copy????'
+    os.system(cmd)  
+
+def openilgms():
+    webbrowser.open_new_tab("https://ilgms.lsgkerala.gov.in/")
+
+def opengptools():
+    webbrowser.open_new_tab("http://gptools.rancorfighters.com")
+
+def opensite():
+    webbrowser.open_new_tab("https://foscan.blogspot.com/")
 
 
 def about():
@@ -73,7 +106,8 @@ txtName = Entry(window)
 lblDocType = Label(window,
                    text="Document type")
 cmbDoctype = ttk.Combobox(window,
-                          values=["Application Form",
+                          values=["Income Certificate",
+                                  "Application Form",
                                   "Ration Card",
                                   "Identity Card",
                                   "Building Plan"])
@@ -82,6 +116,7 @@ radionscan = ttk.Radiobutton(window,
                               text="Normal scan",
                               variable=scanner,
                               value=1)
+"""
 radiotscan = ttk.Radiobutton(window,
                              text="Text scan",
                              variable=scanner,
@@ -90,38 +125,38 @@ radiocscan = ttk.Radiobutton(window,
                              text="Color scan",
                              variable=scanner,
                              value=3)
+"""
 btnNew = ttk.Button(window,
                     text="New Application",
-                    command=newclicked,
-                    state=DISABLED)
+                    command=newclicked,)
 btnScan = ttk.Button(window,
                      text="Scan",
                      command=ScanNow,
                      width=24)
 btnDeleteOldScanned=ttk.Button(window,
                                text="Remove Old Scanned",
-                               state=DISABLED,
+                               command=clearscanned,
                                width=20)
 btnOpenScannedFolder=ttk.Button(window,
                                 text="Open Scanned Folder",
-                                state=DISABLED,
+                                command=openscanned,
                                 width=20)
 btnOpenILGMS=ttk.Button(window,
                         text="Open ILGMS",
                         width=20,
-                        state=DISABLED)
+                        command=openilgms)
 btnOpenMFSearch=ttk.Button(window,
-                           text="Minor Function Search",
+                           text="GP Tools",
                            width=20,
-                           state=DISABLED)
+                           command=opengptools)
 btnAbout=ttk.Button(window,
                     text="About",
                     width=20,
                     command=about)
 btnSettings=ttk.Button(window,
-                   text="Settings",
+                   text="Help / Website",
                    width=20,
-                   state=DISABLED)
+                   command=opensite)
 
 
 # UI
@@ -131,8 +166,8 @@ lblDocType.grid(row=2, column=0, sticky=W)
 cmbDoctype.grid(row=2, column=1, sticky=W)
 radionscan.grid(row=3, column=1, sticky=W)
 scanner.set(1)
-radiotscan.grid(row=4, column=1, sticky=W)
-radiocscan.grid(row=5, column=1, sticky=W)
+#radiotscan.grid(row=4, column=1, sticky=W)
+#radiocscan.grid(row=5, column=1, sticky=W)
 btnNew.grid(row=6, column=0)
 btnScan.grid(row=6, column=1, sticky=W)
 Label(window, text=" ").grid(row=7)
